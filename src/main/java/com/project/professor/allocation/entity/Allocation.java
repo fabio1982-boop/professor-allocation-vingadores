1e8b8ed12e58d409ec20436fcea59f027182345c
fabio <fabiofelixfelix15@hotmail.com>
2025-03-22 18:05:24 -0300

    segundo commit

--- src/main/java/com/project/professor/allocation/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -13,10 +13,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Allocation {
 	@Id
